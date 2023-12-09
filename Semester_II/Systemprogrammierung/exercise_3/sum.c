2c1902c50fbe34b78a04a3cf97293111e1bdc7aa
Liancee <linden.franz@proton.me>
2023-12-09 17:45:33 +0100

    No need for ifndef if -Wfatal-errors is used

--- Semester_II/Systemprogrammierung/exercise_3/sum.c
+++ Semester_II/Systemprogrammierung/exercise_3/sum.c
@@ -13,29 +13,27 @@
   #error "Unknown unsupported operating system"
 #endif
 
-#ifndef Banana_Mac // only that when the compile #error triggers we do not get to see all the resulting syntax errors
-  void usage(char *);
+void usage(char *);
 
-  int main(int args, char *argv[])
-  {
-    if (args != 4) usage(argv[0]);
+int main(int args, char *argv[])
+{
+  if (args != 4) usage(argv[0]);
 
-    // in theory here would be checked if the parameter is valid for its supposed OS, but since it is no requirement for this exercise it is not done.
+  // in theory here would be checked if the parameter is valid for its supposed OS, but since it is no requirement for this exercise it is not done.
 
-    INTEGER_TYPE num1 = atoi(argv[1]);
-    INTEGER_TYPE num2 = atoi(argv[2]);
-    INTEGER_TYPE num3 = atoi(argv[3]);
+  INTEGER_TYPE num1 = atoi(argv[1]);
+  INTEGER_TYPE num2 = atoi(argv[2]);
+  INTEGER_TYPE num3 = atoi(argv[3]);
 
-    INTEGER_TYPE sum = num1 + num2 + num3;
+  INTEGER_TYPE sum = num1 + num2 + num3;
 
-    printf("Sum: %ld\n", (long)sum);
+  printf("Sum: %ld\n", (long)sum);
 
-    return EXIT_SUCCESS;
-  }
+  return EXIT_SUCCESS;
+}
 
-  void usage(char * s)
-  {
-    fprintf(stderr, "Usage: %s <num1> <num2> <num3>\n", s);
-    exit(EXIT_FAILURE);
-  }
-#endif
+void usage(char * s)
+{
+  fprintf(stderr, "Usage: %s <num1> <num2> <num3>\n", s);
+  exit(EXIT_FAILURE);
+}
